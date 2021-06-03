f02a1575f43791482b635d6835ee4dbc4c962e7a
Jenn Greiner <75277268+jenngreiner@users.noreply.github.com>
2021-06-02 20:09:28 -0600

    linking css

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/css/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
