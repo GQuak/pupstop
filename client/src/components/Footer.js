d65a571b7c2adfe647ae810ab5af87e6db58b906
Jenn Greiner <75277268+jenngreiner@users.noreply.github.com>
2021-06-02 21:14:29 -0600

    footer link icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
               <div className="col-3">
                 <a href="https://www.instagram.com/pupstopapp/">
                   <img
-                    src=""
+                    src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/instagram_online_social_media_photo-256.png"
                     className="icon"
                     alt="PupStopApp Instagram"
                   />
@@ -20,7 +20,7 @@ const Footer = () => {
               <div className="col-3">
                 <a href="https://www.facebook.com/PupStopApp/">
                   <img
-                    src=""
+                    src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/online_social_media_facebook-256.png"
                     className="icon"
                     alt="PupStopApp Facebook"
                   />
@@ -29,7 +29,7 @@ const Footer = () => {
               <div className="col-3">
                 <a href="https://www.twitter.com/PupStopApp/">
                   <img
-                    src=""
+                    src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/twitter_online_social_media-256.png"
                     className="icon"
                     alt="PupStopApp Twitter"
                   />
@@ -38,7 +38,7 @@ const Footer = () => {
               <div className="col-3">
                 <a href="mailto:pupstopapp@gmail.com">
                   <img
-                    src=""
+                    src="https://cdn4.iconfinder.com/data/icons/black-white-social-media/32/mail_email_envelope_send_message-512.png"
                     className="icon"
                     alt="PupStopApp Email"
                   />
