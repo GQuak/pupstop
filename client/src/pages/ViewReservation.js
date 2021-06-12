36273bb8cdadb0fafb7ab2aa201a1b627c91d077
Jenn Greiner <75277268+jenngreiner@users.noreply.github.com>
2021-06-12 10:45:38 -0600

    mobile-responsive filter cards

--- client/src/pages/ViewReservation.js
+++ client/src/pages/ViewReservation.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../components/Button";
+import API from "../utils/API";
 
-function ViewReservation({ fname, datetime }) {
+function ViewReservation() {
+  const [reservations, setReservations] = useState([]);
+  const [formObject, setFormObject] = useState({});
+  const [user, setUser] = useState([]);
+
+  const sections = window.location.pathname.split("/");
+  const id = sections[sections.length - 1];
+  console.log(id);
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    API.getReservation(id)
+      .then((res) => {
+        setReservations(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+    // API.getUser(id)
+    //   .then((res) => {
+    //     setUser(res.data);
+    //     console.log("user:", res.data);
+    //   })
+    //   .catch((err) => console.log(err));
+  }, []);
+
   const handleButtonClick = (e) => {
     e.preventDefault();
     window.location.replace("/edit/");
@@ -9,18 +34,16 @@ function ViewReservation({ fname, datetime }) {
 
   return (
     <div>
-      <div class="container">
-        <div class="row">
-          <h3>{fname}'s Reservations</h3>
-        </div>
+      <div className="container">
+        <div className="row">{/* <h3>{user.fname}'s Reservations</h3> */}</div>
 
-        <div class="row">
-          <div class="col-lg-10">
-            <table class="table">
+        <div className="row">
+          <div className="col-lg-10">
+            <table className="table">
               <tbody>
                 <tr>
                   <td>
-                    <a href="/yard/{{yardId}}">{datetime}</a>
+                    <a href="/yard/{{yardId}}">{reservations.datetime}</a>
                   </td>
                   <td>
                     <Button className="default" onClick={handleButtonClick}>
